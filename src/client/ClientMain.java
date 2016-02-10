1d2a3bcd7c9d07aacbdd6be651e2dc0de0cb028b
k-yamamoto-lab <ref1853@gmail.com>
2016-02-10 14:58:17 +0900

    clientの調整

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -1,6 +1,7 @@
 package client;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
@@ -14,17 +15,52 @@ public class ClientMain {
 		int    port     = 2424;
 		if( args.length>0 ) port = Integer.parseInt(args[0]); 
 		socket = new Socket(hostname, port);
-		
+		byte[] buf = new byte[64];
 		Scanner scan = new Scanner(System.in);
 		end: while(true){
-			String buf = scan.next();
-			switch( buf ){
+			System.out.print("command:");
+			String tmp = scan.next();
+			switch( tmp ){
 			case "e":
 			case "exit":
 				break end;
+			case "n":
+			case "name":
+				System.out.print("name:");
+				tmp = scan.next();
+				buf[0] = 0x01;
+				byte[] name = tmp.getBytes("UTF-8");
+				buf[1] = (byte)name.length;
+				System.arraycopy(name, 0, buf, 2, name.length);
+				send( buf, 2+name.length );
+				break;
+			case "j":
+			case "join":
+				buf[0] = 0x02;
+				buf[1] = 0x00;
+				send( buf, 2 );
+				break;
+			case "r":
+			case "ready":
+				buf[0] = 0x03;
+				buf[1] = 0x00;
+				send( buf, 2 );
+				break;
+			case "d":
+			case "debug":
+				buf[0] = 0x09;
+				buf[1] = 0x00;
+				send( buf, 2 );
+				break;
 			}
 		}
-		
+		scan.close();
+	}
+	
+	static void send( byte[] b, int len ) throws IOException{
+		OutputStream os = socket.getOutputStream();
+		os.write(b,0,len);
+		os.flush();
 	}
 
 }

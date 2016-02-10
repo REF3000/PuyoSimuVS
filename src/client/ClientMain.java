ac95ba4a9d914141386a3cdb1b7927d5c9fab21b
'k-yamamoto' <run03a@gmail.com>
2016-02-10 11:40:08 +0900

    clientの基本形

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -1,10 +1,30 @@
 package client;
 
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class ClientMain {
+	
+	static Socket socket;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		String hostname = "localhost";
+		int    port     = 2424;
+		if( args.length>0 ) port = Integer.parseInt(args[0]); 
+		socket = new Socket(hostname, port);
+		
+		Scanner scan = new Scanner(System.in);
+		end: while(true){
+			String buf = scan.next();
+			switch( buf ){
+			case "e":
+			case "exit":
+				break end;
+			}
+		}
+		
 	}
 
 }
